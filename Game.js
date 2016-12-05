87fca9299b8caa5966f2ff88eeeb9d58a079a593
Anto A <antraniga@gmail.com>
2016-12-05 10:22:00 -0800

    8

--- Game.js
+++ Game.js
@@ -1,5 +1,8 @@
 var PlayerX = 250;
 var PlayerY = 250;
+var food01 = document.getElementById("food01");
+var foodEaten = 0
+var timeStart = Date.now()
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37){
         PlayerX -=15;
@@ -29,6 +32,23 @@ document.addEventListener("keydown", function(e) {
 
   if (PlayerX > foodX && PlayerX < foodX + 50 && PlayerY > foodY && PlayerY < foodY + 50) {
       console.log("you did good")
+      foodEaten = foodEaten + 1
+      document.getElementById("score").textContent = foodEaten
+
   }
+
+  if (PlayerX > foodX && PlayerX < foodX + 50 && PlayerY > foodY && PlayerY < foodY + 50) {
+      console.log("you did good")
+      foodEaten = foodEaten + 1
+      document.getElementById("score").textContent = foodEaten
+
+  }
+
+  if(foodEaten == 10) {
+      document.getElementById("canvas").pauseAnimations()
+      var timeStop = Date.now()
+      var duration = Math.round((timeStop - timeStart)/1000)+" seconds"
+  }
+
 
 })
